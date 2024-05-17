76c148c4d5b8fc143ae0f4e903061b99883130f9
Arun <arun.kumar.cd.civ21@itbhu.ac.in>
2024-05-17 23:55:07 +0530

    fixed navbar error1

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -13,10 +13,8 @@ import {
   MobileNavLogo,
   MobileLink,
 } from "./NavbarStyledComponent";
-import { DiCssdeck } from "react-icons/di";
 import { FaBars } from "react-icons/fa";
 import { Bio } from "../../data/constants";
-import { Close, CloseRounded } from "@mui/icons-material";
 import { useTheme } from "styled-components";
 import Logo from "../../images/logo.png";
 
